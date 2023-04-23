06702f43965e1eb1658d4474f324e3dab1a2e040
hielp <hielp@MSI>
2023-04-23 20:17:26 +0800

    Remove Comments

--- AssessmentCP2/src/assessmentcp2/PolyAbsMainGrp8.java
+++ AssessmentCP2/src/assessmentcp2/PolyAbsMainGrp8.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package assessmentcp2;
 
-/**
- *
- * @author Allen James Laxamana
- */
 public class PolyAbsMainGrp8 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       
     }
     
 }
--- AssessmentCP2/src/assessmentcp2/SavingsGrp8.java
+++ AssessmentCP2/src/assessmentcp2/SavingsGrp8.java
@@ -1,14 +1,5 @@
 package assessmentcp2;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Allen James Laxamana
- */
 public class SavingsGrp8 extends AccountGrp8 {
     
 }
