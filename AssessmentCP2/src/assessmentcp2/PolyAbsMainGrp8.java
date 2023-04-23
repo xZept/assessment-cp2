81fb7c3373713e84d915d2f7088aa21c6df688d1
Allen James Laxamana <Allen James Laxamana@LAPTOP-MU0VAU8C>
2023-04-23 21:14:38 +0800

    Write the main class and savings class

--- AssessmentCP2/src/assessmentcp2/PolyAbsMainGrp8.java
+++ AssessmentCP2/src/assessmentcp2/PolyAbsMainGrp8.java
@@ -1,11 +1,30 @@
 package assessmentcp2;
 
-public class PolyAbsMainGrp8 {
+import java.util.Scanner;
 
-    
+public class PolyAbsMainGrp8 {
+            
     public static void main(String[] args) {
-        
-       
-    }
-    
+        Scanner scan = new Scanner(System.in);
+        SavingsGrp8 obj = new SavingsGrp8(); // Object for SavingsGrpX
+        System.out.println("""
+                           Banking Menu
+                           [1] Banking Information
+                           [2] Balance Inquiry
+                           [3] Withdraw
+                           [4] Deposit""");
+        int choice;
+        choice = scan.nextInt();
+        switch (choice) {
+            case 1: 
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+            
+    }  
 }
